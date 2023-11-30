fbf1cbf1650dac081fb43c252828786a70e6acc5
Abhinav Pratap Singh <abhinav.pratap@blumeglobal.com>
2023-11-30 15:45:20 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect, useReducer, useRef } from 'react';
+
 
 function App() {
+
+  const [emotion, setEmotion] = useState("Happy");
+  const [secondary, setSecondary] = useState("Tired");
+  const [checked, setChecked] = useReducer((checked) => !checked, false);
+  
+  useEffect(() => {
+     console.log(`It is ${emotion} around here!`);
+  }, [emotion, secondary]);
+  
+  const txtTitle = useRef();
+  const hexColor = useRef();
+
+  const submit = (e) => {
+        e.preventDefault();
+        const title = txtTitle.current.value;
+        const color = hexColor.current.value;
+        alert(`${title}, ${color}`);
+        txtTitle.current.value = "";
+        hexColor.current.value = "";
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+     <h1>Current emotion is {emotion}</h1>
+     <button onClick={() => setEmotion("Sad")}>Sad</button>
+     <button onClick={() => setEmotion("Excited")}>Excited</button>
+     <h2>Current secondary emotion is {secondary}</h2>
+     <button onClick={() => setSecondary("Grateful")}>Grateful</button>
+     <div>
+        <br></br>
+        <input
+          type="checkbox"
+          value = {checked}
+          onChange={setChecked}
+        />
+        <label>{checked ? "checked" : "not checked"}</label>
+     </div>
+     <br></br>
+      <form onSubmit={submit}>
+        <input
+          ref={txtTitle}
+          type="text"
+          placeholder="color title..."
+        /> 
+        <input ref={hexColor} type = "color"/>
+        <button>ADD</button>
+      </form>
     </div>
-  );
+  ); 
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  //StrictMode is a tool for highlighting any potential problems in your app. 
+  //It's just a package that's going to activate some additional checks to let you know if you've written any code incorrectly, but these are only going to run in development. 
+  //They're not going to run in production.
   <React.StrictMode>
     <App />
   </React.StrictMode>
@@ -14,4 +17,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
